ad51faac7d2359a3aeb0854dc5fe610ad3f4a00a
cassiandrei <cassiano7979>
2018-07-16 07:28:38 -0300

    historico

--- djangoawareness/urls.py
+++ djangoawareness/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from historico import views
+from historico.views import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
