c18d3f1753cf0a216c10bb54eb9354b50b566c2d
cassiandrei <cassiano7979>
2018-07-16 11:26:01 -0300

    historico

--- historico/views.py
+++ historico/views.py
@@ -6,9 +6,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     if request.method == 'POST':
+        print('Entrou POST')
+        print(request.POST)
         Historico.objects.create(temp=request.POST['temp'], feel=request.POST['feels'], dew=request.POST['dew'],
                                  humidity=request.POST['humidity'])
+        print('Criou objeto')
         return HttpResponse(request)
     else:
         historico = Historico.objects.all()
-        return render(request, 'historico/index.html', {'historico': historico})
+        return render(request, 'historico/index.html', {'historico': historico, 'erro': erro})
