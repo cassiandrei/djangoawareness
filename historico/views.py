eae59f0624e0dbceae9432e362eb227f33d3a3fc
cassiandrei <cassiano7979>
2018-07-16 11:34:12 -0300

    historico

--- historico/views.py
+++ historico/views.py
@@ -2,15 +2,17 @@ from django.shortcuts import render
 from .models import Historico
 from django.http import HttpResponse
 
+import logging
 
 # Create your views here.
 def index(request):
     if request.method == 'POST':
-        logging.info("FEZ O POST");
-        logging.info(request.POST);
+        logging.info("FEZ O POST")
+        logging.info(request.POST)
         Historico.objects.create(temp=request.POST['temp'], feel=request.POST['feels'], dew=request.POST['dew'],
                                  humidity=request.POST['humidity'])
         return HttpResponse(request)
     else:
+        logging.info("Nao fez POST")
         historico = Historico.objects.all()
         return render(request, 'historico/index.html', {'historico': historico, 'erro': erro})
